287c69ba36341e2d8ac15a7a3cc4e3d347f7cacc
Rajajaiswal08 <jaiswalraja871@gmail.com>
2024-09-17 15:28:23 +0530

    added change

--- src/Page/Layout.jsx
+++ src/Page/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../Componet/WEbsite/Header'
 import Footer from '../Componet/WEbsite/Footer'
-import Productdata from '../Componet/WEbsite/Productdata'
+
 import Whatsab from '../Componet/WEbsite/Whatsab'
 
 function Layout() {
