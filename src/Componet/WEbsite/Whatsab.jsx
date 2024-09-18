d5bdfb2823d490b56fbc4e384967713a2c7207e1
Rajajaiswal08 <jaiswalraja871@gmail.com>
2024-09-18 12:39:47 +0530

    Build completed successfully

--- src/Componet/WEbsite/Whatsab.jsx
+++ src/Componet/WEbsite/Whatsab.jsx
@@ -6,7 +6,7 @@ function Whatsab() {
     <div>
         <div className="fixed bottom-10 right-10">
   <button className="bg-blue-500 text-white px-6 py-3  rounded-full shadow-lg hover:bg-blue-600">
-    <a href="https://wa.me/9414129269">
+    <a href="https://wa.me/9351361306">
   <FaWhatsapp className='text-[40px]' />
     </a>
   </button>
